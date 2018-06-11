f5dc7501f17626b33fd496259e953fa0e7414185
luc <luc.cano@hotmail.fr>
2018-06-11 14:27:01 +0200

    exercice3

--- exo.js
+++ exo.js
@@ -2,12 +2,20 @@
 
 var toto = 'bonjour';
 alert(toto);
-*/
 
-// Exercice 2  
+
+/* Exercice 2  
 
 var nom = 'Cano';
 var prenom = 'Luc';
 var ville = 'Fleurance';
 
 alert('Nom : '+nom+'\nprenom :'+prenom+'\n ville :'+ville); 
+*/
+
+var reponse = prompt("ton prénom est ?", "<mettre votre prénom ici>");
+if( reponse == null ){
+    alert("Tu as annulé la question :( ");
+} else {
+    alert("Hey " + reponse + ", comment ça va ?");
+}
