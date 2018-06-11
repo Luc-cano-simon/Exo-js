dec41888bafd51b68b6cabdf2faa5bdb0148a2dc
luc <luc.cano@hotmail.fr>
2018-06-11 16:41:44 +0200

    la suite

--- exo.js
+++ exo.js
@@ -13,9 +13,32 @@ var ville = 'Fleurance';
 alert('Nom : '+nom+'\nprenom :'+prenom+'\n ville :'+ville); 
 */
 
+/*
+
 var reponse = prompt("ton prénom est ?", "<mettre votre prénom ici>");
 if( reponse == null ){
     alert("Tu as annulé la question :( ");
 } else {
-    alert("Hey " + reponse + ", comment ça va ?");
+    alert("Hey " + reponse + ", comment ça va ?")
 }
+
+*/
+/*							var rp1 = prompt("ton nom est ?", "<mettre votre nom ici>");
+							var rp2 = prompt("ton prénom  est ?", "<mettre votre prénom ici>");
+							var rp3 = prompt("ta ville est ?", "<mettre votre ville ici>");
+							if( rp1  == null ){
+							    alert("Tu as annulé la question :( ");
+
+							} else {
+								rp2;
+									if( rp1  == null ){
+							    	alert("vous avez annuler")
+							} else {
+								rp3;
+									if( rp2  == null ){
+									alert("vous avez annuler")
+								}
+*/
+
+
+
