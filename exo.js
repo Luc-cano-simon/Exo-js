8ff62aa03e0129db8bf740c26dfab34b69dd686a
luc <luc.cano@hotmail.fr>
2018-06-11 19:00:36 +0200

    exercice 4 et 5

--- exo.js
+++ exo.js
@@ -22,23 +22,40 @@ if( reponse == null ){
     alert("Hey " + reponse + ", comment ça va ?")
 }
 
-*/
-/*							var rp1 = prompt("ton nom est ?", "<mettre votre nom ici>");
-							var rp2 = prompt("ton prénom  est ?", "<mettre votre prénom ici>");
-							var rp3 = prompt("ta ville est ?", "<mettre votre ville ici>");
-							if( rp1  == null ){
-							    alert("Tu as annulé la question :( ");
 
-							} else {
-								rp2;
-									if( rp1  == null ){
-							    	alert("vous avez annuler")
-							} else {
-								rp3;
-									if( rp2  == null ){
-									alert("vous avez annuler")
+									EXO 4 */
+/*
+var rp1 = prompt("ton nom est ?", "<mettre votre nom ici>");
+var rp2 = prompt("ton prénom  est ?", "<mettre votre prénom ici>");
+var rp3 = prompt("ta ville est ?", "<mettre votre ville ici>");
+	if( rp1  == null ){
+		alert("Tu as annulé la question :( ");
+
+} else {
+	rp2;
+	if( rp1  == null ){
+		alert("vous avez annuler")
+} else {
+		rp3;
+	if( rp2  == null ){
+		alert("vous avez annuler")
 								}
+
+*/
+											/* Exercice 4 Bis 
+
+
+var nom= prompt("Quel est ton nom?");
+var prenom = prompt("Quel est ton prenom?");
+var ville = prompt("Où habite-tu?");
+alert("Bonjour, "+ prenom + " " + nom + " tu habite à " + ville);
+
 */
 
 
-
+var nb1 = prompt("Choisissez un nombre à virgule");
+var nb2 = prompt("Choisissez un second nombre à virgule");
+nb1=parseInt(nb1);
+nb2=parseInt(nb2);
+var result = nb1 * nb2;
+alert("le resultat de la multiplication est : "+result);
