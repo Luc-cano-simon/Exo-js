fda95cc7da1fb0505641d06a7483321fbb88036b
luc <luc.cano@hotmail.fr>
2018-06-11 14:07:54 +0200

    exercice2

--- exo.js
+++ exo.js
@@ -1,2 +1,13 @@
-var toto = 'bonjour'
+/* Exercice 1 
+
+var toto = 'bonjour';
 alert(toto);
+*/
+
+// Exercice 2  
+
+var nom = 'Cano';
+var prenom = 'Luc';
+var ville = 'Fleurance';
+
+alert('Nom : '+nom+'\nprenom :'+prenom+'\n ville :'+ville); 
